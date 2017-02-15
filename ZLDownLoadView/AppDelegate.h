226c5aeb62ce41d74277d2291eb6d8b157c13d13
LeLe <1149733675@qq.com>
2017-02-15 16:45:01 +0800

    Initial Commit

---
+++ ZLDownLoadView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZLDownLoadView
+//
+//  Created by 趙乐樂 on 2017/2/15.
+//  Copyright © 2017年 趙乐樂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZLDownLoadView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZLDownLoadView
+//
+//  Created by 趙乐樂 on 2017/2/15.
+//  Copyright © 2017年 趙乐樂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
